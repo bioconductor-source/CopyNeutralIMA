180a067698f1d91e41ca0e78bbf6b5c7d4939c0a
Pastor Hostench <x.pastorhostench@dkfz-heidelberg.de>
2018-06-28 18:31:53 +0200

    Updated BiocVersion; changed Rdata names

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,13 +1,13 @@
 meta <- data.frame(
-  Title = c("Copy neutral samples from InfiniumHumanMethylation450k arrays in GEO",
-            "Copy neutral samples from InfiniumHumanMethylationEPIC arrays in GEO"),
+  Title = c("Copy neutral samples from IlluminaHumanMethylation450k arrays in GEO",
+            "Copy neutral samples from IlluminaHumanMethylationEPIC arrays in GEO"),
   Description = c("51 InfiniumHumanMethylation450k samples. The provided controls consist of material from healthy individuals with nominally no copy number abberations.",
                   "13 InfiniumHumanMethylationEPIC samples. The provided controls consist of material from healthy individuals with nominally no copy number abberations."),
-  BiocVersion = rep("3.6",2),
+  BiocVersion = rep("3.8",2),
   Genome = rep("hg19",2),
   SourceType = rep("IDAT",2),
   SourceUrl = rep("http://www.ncbi.nlm.nih.gov/geo/",2),
-  SourceVersion = format(Sys.time(), "%b %d %Y")
+  SourceVersion = format(Sys.time(), "%b %d %Y"),
   Species = rep("Homo sapiens",2),
   TaxonomyId = rep(9606,2),
   Coordinate_1_based = rep(TRUE,2),
@@ -15,7 +15,7 @@ meta <- data.frame(
   Maintainer = rep("xpastor79@gmail.com",2),
   RDataClass = rep("RGChannelSetExtended",2),
   DispatchClass = rep("Rda",2),
-  RDataPath = file.path("CopyNeutralIMA/data", c("InfiniumHumanMethylation450k.rda","InfiniumHumanMethylationEPIC.rda"))
+  RDataPath = file.path("CopyNeutralIMA/data", c("IlluminaHumanMethylation450k.rda","IlluminaHumanMethylationEPIC.rda"))
 )
 
 # write metadata.csv file
